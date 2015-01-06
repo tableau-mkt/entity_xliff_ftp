0ac3009c1205ab70e2293a38bc78da3d511dc813
Eric Peterson <epeterson@tableausoftware.com>
2015-01-05 17:46:58 -0800

    Adding tests for TableauWorldServer\Querier class. re #3556

---
+++ test/QuerierTest.php
@@ -0,0 +1,335 @@
+<?php
+
+namespace TableauWorldServer\Tests;
+
+use TableauWorldServer\Querier;
+
+class QuerierTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Tests that the Querier constructor will throw an exception in the case
+   * that an SFTP client is passed in that is not connected / logged in.
+   *
+   * @test
+   * @expectedException \Exception
+   * @expectedExceptionMessage The provided SFTP client must already be connected.
+   */
+  public function constructorExpectsConnection() {
+    $mockClient = $this->getMockBuilder('Net_SFTP')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $querier = new Querier($mockClient);
+  }
+
+  /**
+   * @test
+   */
+  public function getProcessableGetsDefaultInstalledLanguages() {
+    $mockClient = $this->getConnectedClientMock();
+    $observerDrupal = $this->getMock('TableauWorldServer\Utils\DrupalHandler');
+
+    // We expect that DrupalHandler::languageList() will be called exactly once.
+    $observerDrupal->expects($this->once())
+      ->method('languageList')
+      ->with($this->equalTo('language'))
+      ->willReturn(array());
+
+    // Instantiate Querier and call getProcessable without languages.
+    $querier = new Querier($mockClient, $observerDrupal);
+    $querier->getProcessable();
+  }
+
+  /**
+   * @test
+   */
+  public function getProcessableIgnoresEnglish() {
+    $observerClient = $this->getConnectedClientMock();
+    $observerDrupal = $this->getMock('TableauWorldServer\Utils\DrupalHandler');
+
+    // We expect that DrupalHandler::languageList() will never be called.
+    $observerDrupal->expects($this->never())
+      ->method('languageList');
+
+    // We expect that Net_SFTP::rawList will never be called.
+    $observerClient->expects($this->never())
+      ->method('rawList');
+
+    // Instantiate Querier and call getProcessable with ONLY English.
+    $querier = new Querier($observerClient, $observerDrupal);
+    $querier->getProcessable(array('en' => (object) array()));
+  }
+
+  /**
+   * @test
+   */
+  public function getProcessableReturnsResults() {
+    $expectedSourceRoot = 'path/to/source';
+    $languageList = $this->getValidLangObjects();
+    $expectedFileList = array(
+      'node-1.xlf' => array('type' => 1),
+      'unmanaged-file.xlf' => array('type' => 1),
+      'unfortunately_named_directory-1.xlf' => array('type' => 2),
+    );
+    $expectedResponse = array(
+      'fr' => array('node' => array(1 => 1)),
+      'de' => array('node' => array(1 => 1)),
+    );
+
+    $observerClient = $this->getConnectedClientMock();
+    $observerDrupal = $this->getMock('TableauWorldServer\Utils\DrupalHandler');
+
+    // We expect that DrupalHandler::variableGet() will be called exactly once.
+    $observerDrupal->expects($this->once())
+      ->method('variableGet')
+      ->with(Querier::SOURCEROOTVAR)
+      ->willReturn($expectedSourceRoot);
+
+    // We expect that DrupalHandler::languageList() will be called exactly once.
+    $observerDrupal->expects($this->once())
+      ->method('languageList')
+      ->with($this->equalTo('language'))
+      ->willReturn($languageList);
+
+    // We expect that Net_SFTP::rawList will be called as many times as there
+    // are languages.
+    $observerClient->expects($this->exactly(count($languageList)))
+      ->method('rawList')
+      ->withConsecutive(
+        array($this->equalTo($expectedSourceRoot . '/fr-FR')),
+        array($this->equalTo($expectedSourceRoot . '/de-DE'))
+      )
+      ->willReturn($expectedFileList);
+
+    // Instantiate Querier and call getProcessable with ONLY English.
+    $querier = new Querier($observerClient, $observerDrupal);
+    $this->assertEquals($expectedResponse, $querier->getProcessable());
+  }
+
+  /**
+   * @test
+   */
+  public function getProcessableByEntity() {
+    $expectedLangs = $this->getValidLangObjects();
+    $expectedResponse = array('node' => array(1 => array('fr' => 'fr', 'de' => 'de')));
+
+    $observerQuerier = $this->getMockBuilder('TableauWorldServer\Querier')
+      ->disableOriginalConstructor()
+      ->setMethods(array('getProcessable'))
+      ->getMock();
+
+    // We expect that Querier::getProcessable() will be called exactly once.
+    $observerQuerier->expects($this->once())
+      ->method('getProcessable')
+      ->with($expectedLangs)
+      ->willReturn(array(
+        'fr' => array('node' => array(1 => 1)),
+        'de' => array('node' => array(1 => 1)),
+      ));
+
+    // Call Querier::getProcessable(), validate expectations.
+    $this->assertEquals($expectedResponse, $observerQuerier->getProcessableByEntity($expectedLangs));
+  }
+
+  /**
+   * @test
+   */
+  public function getProcessedGetsDefaultInstalledLanguages() {
+    $mockClient = $this->getConnectedClientMock();
+    $observerDrupal = $this->getMock('TableauWorldServer\Utils\DrupalHandler');
+
+    // We expect that DrupalHandler::languageList() will be called exactly once.
+    $observerDrupal->expects($this->once())
+      ->method('languageList')
+      ->with($this->equalTo('language'))
+      ->willReturn(array());
+
+    // Instantiate Querier and call getProcessable without languages.
+    $querier = new Querier($mockClient, $observerDrupal);
+    $querier->getProcessed();
+  }
+
+  /**
+   * @test
+   */
+  public function getProcessedIgnoresEnglish() {
+    $observerClient = $this->getConnectedClientMock();
+    $observerDrupal = $this->getMock('TableauWorldServer\Utils\DrupalHandler');
+
+    // We expect that DrupalHandler::languageList() will never be called.
+    $observerDrupal->expects($this->never())
+      ->method('languageList');
+
+    // We expect that Net_SFTP::rawList will never be called.
+    $observerClient->expects($this->never())
+      ->method('rawList');
+
+    // Instantiate Querier and call getProcessable with ONLY English.
+    $querier = new Querier($observerClient, $observerDrupal);
+    $querier->getProcessed(array('en' => (object) array()));
+  }
+
+  /**
+   * @test
+   */
+  public function getProcessedReturnsResults() {
+    $expectedSourceRoot = 'path/to/source';
+    $languageList = $this->getValidLangObjects();
+    $expectedFileList = array(
+      'node-1.xlf' => array(
+        'type' => 1,
+        'filename' => 'node-1.xlf',
+        'size' => 12345,
+        'mtime' => 98765,
+        'atime' => 43210,
+      ),
+      'unmanaged-file.xlf' => array(
+        'type' => 1
+      ),
+      'unfortunately_named_directory-1.xlf' => array(
+        'type' => 2
+      ),
+    );
+    $expectedResponse = array(
+      'fr' => array('node' => array(1 => array(
+        'filename' => 'node-1.xlf',
+        'size' => 12345,
+        'modified' => 98765,
+        'created' => 43210,
+      ))),
+      'de' => array('node' => array(1 => array(
+        'filename' => 'node-1.xlf',
+        'size' => 12345,
+        'modified' => 98765,
+        'created' => 43210,
+      ))),
+    );
+
+    $observerClient = $this->getConnectedClientMock();
+    $observerDrupal = $this->getMock('TableauWorldServer\Utils\DrupalHandler');
+
+    // We expect that DrupalHandler::variableGet() will be called exactly once.
+    $observerDrupal->expects($this->once())
+      ->method('variableGet')
+      ->with(Querier::SOURCEROOTVAR)
+      ->willReturn($expectedSourceRoot);
+
+    // We expect that DrupalHandler::languageList() will be called exactly once.
+    $observerDrupal->expects($this->once())
+      ->method('languageList')
+      ->with($this->equalTo('language'))
+      ->willReturn($languageList);
+
+    // We expect that Net_SFTP::rawList will be called as many times as there
+    // are languages.
+    $observerClient->expects($this->exactly(count($languageList)))
+      ->method('rawList')
+      ->withConsecutive(
+        array($this->equalTo($expectedSourceRoot . '/fr-FR/processed')),
+        array($this->equalTo($expectedSourceRoot . '/de-DE/processed'))
+      )
+      ->willReturn($expectedFileList);
+
+    // Instantiate Querier and call getProcessable with ONLY English.
+    $querier = new Querier($observerClient, $observerDrupal);
+    $this->assertEquals($expectedResponse, $querier->getProcessed());
+  }
+
+  /**
+   * @test
+   */
+  public function getProcessedByEntity() {
+    $expectedLangs = $this->getValidLangObjects();
+    $expectedResponse = array(
+      'node' => array(
+        1 => array(
+          'fr' => array(
+            'filename' => 'node-1.xlf',
+            'size' => 12345,
+            'modified' => 98765,
+            'created' => 43210,
+          ),
+          'de' => array(
+            'filename' => 'node-1.xlf',
+            'size' => 12345,
+            'modified' => 98765,
+            'created' => 43210,
+          ),
+        )));
+
+    $observerQuerier = $this->getMockBuilder('TableauWorldServer\Querier')
+      ->disableOriginalConstructor()
+      ->setMethods(array('getProcessed'))
+      ->getMock();
+
+    // We expect that Querier::getProcessable() will be called exactly once.
+    $observerQuerier->expects($this->once())
+      ->method('getProcessed')
+      ->with($expectedLangs)
+      ->willReturn(array(
+        'fr' => array('node' => array(1 => array(
+          'filename' => 'node-1.xlf',
+          'size' => 12345,
+          'modified' => 98765,
+          'created' => 43210,
+        ))),
+        'de' => array('node' => array(1 => array(
+          'filename' => 'node-1.xlf',
+          'size' => 12345,
+          'modified' => 98765,
+          'created' => 43210,
+        ))),
+      ));
+
+    // Call Querier::getProcessable(), validate expectations.
+    $this->assertEquals($expectedResponse, $observerQuerier->getProcessedByEntity($expectedLangs));
+  }
+
+  /**
+   * @dataProvider fileNameProvider
+   * @test
+   */
+  public function parseFileName($filename, $expectedResponse) {
+    $mockClient = $this->getConnectedClientMock();
+    $querier = new Querier($mockClient);
+    $this->assertEquals($expectedResponse, $querier->parseFilename($filename));
+  }
+
+  public function fileNameProvider() {
+    return array(
+      array('node-123.xlf', array('type' => 'node', 'identifier' => 123)),
+      array('taxonomy_term-999999.xlf', array('type' => 'taxonomy_term', 'identifier' => 999999)),
+      array('Not-real-111.xlf', array()),
+      array('12345-node.xlf', array()),
+      array('Another file', array()),
+    );
+  }
+
+  /**
+   * Returns a mock SFTP client that is connected.
+   * @return \PHPUnit_Framework_MockObject_MockObject
+   */
+  protected function getConnectedClientMock() {
+    $mockClient = $this->getMockBuilder('Net_SFTP')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $mockClient->expects($this->any())
+      ->method('isConnected')
+      ->willReturn(TRUE);
+    return $mockClient;
+  }
+
+  /**
+   * Returns an array of valid Drupal language objects (or at least valid
+   * insofar as MiddleWare needs it to be).
+   *
+   * @return object[]
+   */
+  protected function getValidLangObjects() {
+    return array(
+      'fr' => (object) array('prefix' => 'fr-fr', 'name' => 'French'),
+      'de' => (object) array('prefix' => 'de-de', 'name' => 'German'),
+    );
+  }
+
+}
